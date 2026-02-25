6b0f8822f3537c9a15e201f835e4508d0ff88272
Abdessamad Elbouazzaoui <aelbouaz@f1r3s3.codam.nl>
2026-02-25 19:31:35 +0100

    table generated perfectly

--- CPP_01/phonebook.cpp
+++ CPP_01/phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: aelbouaz <aelbouaz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/25 16:07:05 by aelbouaz          #+#    #+#             */
-/*   Updated: 2026/02/25 18:11:25 by aelbouaz         ###   ########.fr       */
+/*   Updated: 2026/02/25 19:30:38 by aelbouaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,21 +45,41 @@ void	add_option(PhoneBook *phonebook, int i)
 	phonebook->contact[i].DarkestSecret = darkest;
 }
 
+void	print_row(std::string str)
+{
+	int	diff = 10 - str.length();
+	if (diff >= 0)
+	{
+		std::cout<<"|";
+		for(int i = diff; i > 0; --i)
+			std::cout<<" ";
+		std::cout<<str;
+	}
+	else if (diff < 0)
+	{
+		std::cout<<"|";
+		for(int k = 0; k < 9; ++k)
+			std::cout<< str[k];
+		std::cout<< ".";
+	}
+}
+
 void	search_option(PhoneBook *phonebook)
 {
-	std::cout<<"|‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾|\n";
-	std::cout<<"|     index| firstname|  lastname| nickname|\n";
-	std::cout<<"|‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾|\n";
-	for (int i = 0; i < 8; ++i)
+	std::cout<<"|‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾|\n";
+	std::cout<<"|     index| firstname|  lastname|  nickname|\n";
+	std::cout<<"|‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾|\n";
+	for (int i = 0; i < 8; i++)
 	{
 		std::cout<< "|         " << phonebook->contact[i].Index;
-		std::cout<< "|       " << phonebook->contact[i].FirstName;
-		std::cout<< "|       " << phonebook->contact[i].LastName;
-		std::cout<< "|       " << phonebook->contact[i].NickName << "|\n" ;
+		print_row(phonebook->contact[i].FirstName);
+		print_row(phonebook->contact[i].LastName);
+		print_row(phonebook->contact[i].NickName);
+		std::cout<<"|\n";
 		if (i != 7)
-			std::cout<<"|‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾|\n";
+			std::cout<<"|‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾|\n";
 		else
-			std::cout<<"‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n";
+			std::cout<<"‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n";
 	}
 }
 
@@ -75,18 +95,22 @@ int main(void)
 	std::cout<<"| |_) | '_ \\ / _ \\|  _ \\ / _ \\ |_ \\ / _ \\ / _ \\| |/ /\n";
 	std::cout<<"|  __/| | | | (_) | | | |  __/ |_) | (_) | (_) |   <\n";
 	std::cout<<"|_|   |_| |_|\\___/|_| |_|\\___|_.__/ \\___/ \\___/|_|\\_\\ \n\n";
+	for (int k = 0; k < 8; ++k)
+		phonebook.contact[k].Index = k;
 	while (1)
 	{
 		std::cout<<"Please enter one of the options: ADD/SEARCH/EXIT\n";
 		std::cin >> identifer;
 		if (!identifer.compare("ADD") || !identifer.compare("add"))
+		{
 			add_option(&phonebook, i);
+			if (i < 8)
+				i++;
+		}
 		else if (!identifer.compare("SEARCH") || !identifer.compare("search"))
 			search_option(&phonebook);
 		else if (!identifer.compare("EXIT") || !identifer.compare("exit"))
 			break ;
-		if (i < 7)
-			i++;
 	}
 	return (0);
 }
