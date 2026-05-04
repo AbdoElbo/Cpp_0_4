273bf7becc45ce69e288d9beee480763223fa690
Abdessamad Elbouazzaoui <aelbouaz@f1r2s9.codam.nl>
2026-05-04 18:05:47 +0200

    first ex00 sucks

---
+++ CPP_02/ex00/Fixed.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelbouaz <aelbouaz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/29 18:53:13 by aelbouaz          #+#    #+#             */
+/*   Updated: 2026/05/04 14:35:01 by aelbouaz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <string>
+#include <iostream>
+
+class Fixed
+{
+	private:
+		int					integer;
+		static const int	cstInt = 8;
+	public:
+		Fixed();
+		Fixed(const Fixed& other);
+		Fixed& operator=(const Fixed& other);
+		~Fixed();
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+};
+
+
---
+++ CPP_02/ex00/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelbouaz <aelbouaz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/20 19:21:28 by aelbouaz          #+#    #+#             */
+/*   Updated: 2026/04/30 16:05:25 by aelbouaz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main()
+{
+	Fixed a;
+	Fixed b(a);
+	Fixed c;
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	return EXIT_SUCCESS;
+}
+
